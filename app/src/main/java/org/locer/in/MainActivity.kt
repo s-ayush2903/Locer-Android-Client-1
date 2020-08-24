74b84a079d0ad111b9dff567d9cf767d0b7a3aab
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2020-08-24 11:40:15 +0530

    Code Cleanup(test it)

--- app/src/main/java/org/locer/in/MainActivity.kt
+++ app/src/main/java/org/locer/in/MainActivity.kt
@@ -2,26 +2,23 @@ package org.locer.`in`
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ActionMode
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.locer.`in`.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var sharedPreferenceUtil: SharedPreferenceUtil
+    private  val sharedPreferenceUtil by lazy { SharedPreferenceUtil(context = this) }
     private lateinit var boundLayout: ActivityMainBinding
     private lateinit var bottomNavView: BottomNavigationView
     private lateinit var appBarConfig: AppBarConfiguration
-    private lateinit var navController :NavController
+    private lateinit var navController: NavController
     private var actionMode: ActionMode? = null
     private val finishActionModeOnDestinationChanged =
         NavController.OnDestinationChangedListener { controller, destination, bundle -> actionMode?.finish() }
@@ -29,21 +26,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sharedPreferenceUtil = SharedPreferenceUtil(context = this)
-        if (sharedPreferenceUtil.showIntro()) {
-            val homeIntent = Intent(this, IntroActivity::class.java)
-            startActivity(homeIntent)
-            sharedPreferenceUtil.setIntroShown()
-            finish()
-        } else {
+
+        if (!sharedPreferenceUtil.showIntro()) {
             boundLayout = DataBindingUtil.setContentView(this, R.layout.activity_main)
             bottomNavView = boundLayout.mainBottomNavView
             navController = Navigation.findNavController(this, R.id.main_nav_host_fragment)
             navController.addOnDestinationChangedListener(finishActionModeOnDestinationChanged)
             appBarConfig = AppBarConfiguration(navController.graph)
             boundLayout.mainBottomNavView.setupWithNavController(navController)
-
+        } else {
+            handleIntro()
         }
+    }
+
+    private fun handleIntro() {
+        val homeIntent = Intent(this, IntroActivity::class.java)
+        startActivity(homeIntent)
+        sharedPreferenceUtil.setIntroShown()
+        finish()
     }
 
     private fun setUpNavMenus(navController: NavController) {
