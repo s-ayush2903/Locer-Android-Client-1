3f95de96e84e1c752b29472fa2fcbf2bc8de0ccf
Raghav Sharma <59222356+raghavSharma1472@users.noreply.github.com>
2020-10-04 11:34:26 +0530

    Update Affirmation.kt

--- app/src/main/java/org/locer/in/recyclerView/Affirmation.kt
+++ app/src/main/java/org/locer/in/recyclerView/Affirmation.kt
@@ -1,4 +1,5 @@
 package org.locer.`in`.recyclerView
-
+/*
+This class is basically a data model class. We will model our data here, for now I have just kept few strings
+*/
 data class Affirmation (val stringResourceId: Int)
-//This class is basically a data model class. We will model our data here, for now I have just kept few strings
