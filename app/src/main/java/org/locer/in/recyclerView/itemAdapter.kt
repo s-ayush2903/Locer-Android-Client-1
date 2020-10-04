a81a9d3f12193e3a71742ff86d0023dbd6eacce4
Raghav Sharma <59222356+raghavSharma1472@users.noreply.github.com>
2020-10-04 11:38:27 +0530

    Update itemAdapter.kt

--- app/src/main/java/org/locer/in/recyclerView/itemAdapter.kt
+++ app/src/main/java/org/locer/in/recyclerView/itemAdapter.kt
@@ -1,5 +1,5 @@
 package org.locer.`in`.recyclerView
-
+//This class is the adapter i.e. brain of how recycler view will look
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +7,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.locer.`in`.HomeFragment
 import org.locer.`in`.R
-//This class is the adapter i.e. brain of how recycler view will look
 class ItemAdapter(private val context: HomeFragment, private val dataset: List<Affirmation>) :  RecyclerView.Adapter<ItemAdapter.ItemViewHolder>()  {
     //This class is holding all the views that we want to display in a container of recycler view
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
@@ -15,11 +14,9 @@ class ItemAdapter(private val context: HomeFragment, private val dataset: List<A
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item, parent, false)
-
         return ItemViewHolder(
-            adapterLayout
+            LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
         )
     }
 
