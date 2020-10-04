1a1f96cad8dbbb2883a9965e839fb288becfc0e7
raghavSharma1472 <grandraghav@outlook.com>
2020-10-04 12:48:55 +0530

    Comments Signified

--- app/src/main/java/org/locer/in/recyclerView/Affirmation.kt
+++ app/src/main/java/org/locer/in/recyclerView/Affirmation.kt
@@ -1,5 +1,7 @@
 package org.locer.`in`.recyclerView
-/*
+
+/**
 This class is basically a data model class. We will model our data here, for now I have just kept few strings
-*/
-data class Affirmation (val stringResourceId: Int)
+ */
+
+data class Affirmation(val stringResourceId: Int)
